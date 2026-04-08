1a10dfd013fd98cf10a6a3a047350811578ff174
RLagos <72813751+RCHLEngene@users.noreply.github.com>
2026-04-08 05:26:35 -0500

    Change Spark streaming source to Kafka
    Updated the Spark streaming application to read from Kafka instead of a CSV stream. Added data transformation to separate columns from the Kafka message.

--- consumer/spark_streaming.py
+++ consumer/spark_streaming.py
@@ -1,11 +1,30 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import split, col
 
-spark = SparkSession.builder.appName("StreamingExample").getOrCreate()
+spark = SparkSession.builder \
+    .appName("KafkaSparkStreaming") \
+    .getOrCreate()
 
-# Leer stream (simulado con carpeta)
-df_stream = spark.readStream.csv("stream_data/", header=True)
+# Leer desde Kafka
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "ventas") \
+    .load()
 
-query = df_stream.writeStream \
+# Convertir a texto
+df = df.selectExpr("CAST(value AS STRING)")
+
+# Separar columnas
+df = df.withColumn("data", split(col("value"), ",")) \
+       .select(
+           col("data")[0].alias("Producto"),
+           col("data")[1].alias("Cantidad"),
+           col("data")[2].alias("Precio")
+       )
+
+# Mostrar en consola
+query = df.writeStream \
     .outputMode("append") \
     .format("console") \
     .start()
